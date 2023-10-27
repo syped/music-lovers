cd83e6fcc30fcba928dec4ffcad471a78ef43807
Mujahid <mujahidghazalgoo@gmail.com>
2023-10-26 22:45:57 -0400

    css

--- react-app/src/components/AlbumPage/index.js
+++ react-app/src/components/AlbumPage/index.js
@@ -1,7 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import { getAlbums } from "../../store/album";
+import { useHistory } from "react-router-dom";
+import "./AlbumPage.css";
 
 function AllAlbums() {
+  const history = useHistory();
   const dispatch = useDispatch();
 
   const allAlbumsObj = useSelector((state) => state.albums.allAlbums);
@@ -15,15 +18,24 @@ function AllAlbums() {
 
   return (
     <>
-      <div>
-        {arr.map((album) => (
-          // navlink to album details page
-          <div className="album-card">
-            <img src={album.album_image} />
-            <div>{album.album_name}</div>
-            <div>{album.release_year}</div>
-          </div>
-        ))}
+      <div className="album-body-card">
+        <div className="main-album-card">
+          {arr.map((album) => (
+            // navlink to album details page
+            <div
+              className="album-card"
+              onClick={() => {
+                history.push(`/albums/${album.id}`);
+              }}
+            >
+              <img className="album-page-img" src={album.album_image} />
+              <div className="solo-album-info-card">
+                <div className="album-page-name">{album.album_name}</div>
+                <div className="album-year">{album.release_year}</div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
