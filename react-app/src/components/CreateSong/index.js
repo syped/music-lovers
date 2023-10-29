399e572ca43757ec82a3bd783514f99f352b7272
camille <huangcamillej@gmail.com>
2023-10-29 16:07:23 -0700

    AAAAAAAAHHHHHHHHHHH

--- react-app/src/components/CreateSong/index.js
+++ react-app/src/components/CreateSong/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { useModal } from "../../context/Modal";
 import { createSongThunk } from "../../store/song";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import "./CreateSong.css";
 
 function CreateSongForm({ submitted, albumId }) {
   const dispatch = useDispatch();
@@ -14,6 +15,7 @@ function CreateSongForm({ submitted, albumId }) {
   const [mp3, setMp3] = useState(null);
   const [mp3Loading, setMp3Loading] = useState(false);
   const [errors, setErrors] = useState({});
+  const { closeModal } = useModal();
 
   function errorsChecked(name, mp3) {
     const errors = {};
@@ -37,14 +39,6 @@ function CreateSongForm({ submitted, albumId }) {
 
     setMp3Loading(true);
 
-    // const newSong = {
-    //   user_id: userId,
-    //   album_id: 1, //HARD-CODED, FIX TO ALBUM NAME
-    //   song_name: name,
-    //   length: length,
-    //   mp3: mp3,
-    // };
-
     if (Object.keys(errorsFound).length === 0) {
       const response = await dispatch(createSongThunk(formData));
       setMp3Loading(false);
@@ -62,47 +56,54 @@ function CreateSongForm({ submitted, albumId }) {
     }
   };
 
+  const handleClose = () => {
+    closeModal();
+  };
+
   return (
-    <div className="main-create-song-container">
-      <div className="create-song-container">
-        <div className="create-song-form">
-          <div className="upload-song-title-container">
-            <h1 className="upload-song-title">Upload your Song(s)</h1>
-          </div>
-          <form onSubmit={handleSubmit} encType="multipart/form-data">
-            <div className="song-form-fields">
-              <label>
-                Song Name:
-                <div className="song-name-box">
-                  <input
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    placeholder="Song Name"
-                  />
-                </div>
-              </label>
-              {errors.name && <p className="errors">{errors.name}</p>}
-            </div>
-            <div className="song-form-fields">
-              <label>
-                Song File:
-                <div className="song-file-box">
-                  <input
-                    type="file"
-                    accept="audio/*"
-                    onChange={(e) => setMp3(e.target.files[0])}
-                  />
-                </div>
-                {mp3Loading && <p>Loading...</p>}
-              </label>
-              {errors.name && <p className="errors">{errors.mp3}</p>}
-            </div>
-            <div className="create-song-button">
-              <button type="submit">Upload Song</button>
-            </div>
-          </form>
+    <div className="main-edit-song-container">
+      <button className="close-button" onClick={handleClose}>
+        X
+      </button>
+      <div className="update-song-form">
+        <div className="update-song-title">
+          <h1>Upload Your Song</h1>
         </div>
+        <form
+          className="uploading-songs"
+          onSubmit={handleSubmit}
+          encType="multipart/form-data"
+        >
+          <div className="upload-song-form-fields">
+            <label>
+              Song Name:
+              <div className="song-name-box">
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Song Name"
+                />
+              </div>
+            </label>
+            {errors.name && <p className="errors">{errors.name}</p>}
+          </div>
+
+          <div className="upload-song-file-fields">
+            <label>Song File:</label>
+            <input
+              type="file"
+              accept="audio/*"
+              onChange={(e) => setMp3(e.target.files[0])}
+            />
+
+            {errors.mp3 && <p className="errors">{errors.mp3}</p>}
+          </div>
+
+          <div className="upload-song-button">
+            <button type="submit">Upload Song</button>
+          </div>
+        </form>
       </div>
     </div>
   );
