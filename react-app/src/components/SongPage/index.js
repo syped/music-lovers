46b51b5ec3d60ddb2f51a57e20952aa405e83e7f
robert <robertly53@gmail.com>
2023-10-29 19:07:47 -0400

    updated all songs

--- react-app/src/components/SongPage/index.js
+++ react-app/src/components/SongPage/index.js
@@ -2,11 +2,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { getSongsThunk } from "../../store/song";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
+import { getAlbums } from "../../store/album";
+import { useEffect } from "react";
 
 function AllSongs({ selectedSong, selectedList }) {
   const dispatch = useDispatch();
 
   const allSongsObj = useSelector((state) => state.songs.allSongs);
+  const allAlbums = useSelector((state) => state.albums.allAlbums);
 
   const arr = Object.values(allSongsObj);
 
@@ -28,29 +31,38 @@ function AllSongs({ selectedSong, selectedList }) {
 
   return (
     <>
-      <div className="single-album-container">
+      <h1 className="all-songs-header">All Songs</h1>
+      <div className="song-page-container">
         <h3 className="single-album-header">Title</h3>
-        <div className="underline"></div>
+        <h3 className="songs-album-header">Album</h3>
+        <div className="underline-song"></div>
         <div className="song-container">
           {arr.map((song) => (
-            <div className="single-album-song">
+            <div className="single-song">
               {/* add picture */}
+              <img
+                className="song-card-album-cover"
+                src={allAlbums[song.album_id].album_image}
+              />
               <img
                 onClick={() => {
                   handleClick(song.id); //ADDDDDDDDDDD SONGGG
                   selectedList(arr);
                 }}
-                className="song-play-playlist"
+                className="song-play-song"
                 src={playIcon}
               />
               <div>{song.song_name}</div>
-              <div>{song.user_id}</div>
+
               {/* add .firstName to song.user_id */}
-              <div>{song.album_id}</div>
+              <div className="songs-album-name">
+                {allAlbums[song.album_id] &&
+                  allAlbums[song.album_id].album_name}
+              </div>
             </div>
           ))}
         </div>
-        <div className="single-album-song-background"></div>
+        <div className="song-page-background"></div>
       </div>
     </>
   );
